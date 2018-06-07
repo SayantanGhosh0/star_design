ef06d3a4817957df3582b95bf8014b2dd9d803ec
SayantanGhosh0 <giichi088@gmail.com>
2018-06-07 10:18:14 +0530

    Add files via upload

---
+++ FirstStarDesign.py
@@ -0,0 +1,23 @@
+print("Welcome to my code")
+#at = "hi there"
+#at = "".join(reversed(at))
+#print(at)
+
+#at = at.split(' ')
+
+#print(at[0])
+
+import turtle
+
+star = turtle.Turtle()
+def st():
+  for i in range(5):
+    star.forward(150)
+    star.right(144)
+
+
+for i in range(18):
+    st()
+    star.right(20)
+
+input("hi")
